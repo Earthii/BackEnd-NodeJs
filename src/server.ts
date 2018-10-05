02b9654c1a2226c5ed008a298fb9fcfa72cd7fb4
Earthii <xiao_eric@hotmail.com>
2018-10-05 01:47:45 -0400

    ?

--- src/server.ts
+++ src/server.ts
@@ -8,25 +8,27 @@ import { WelcomeController } from './controllers';
 const app: express.Application = express();
 const port: string | number = process.env.PORT || 3000;
 
-// app.use(bodyParser.json()); // parse application/json
-// app.use(
-//   // parse application/x-www-form-urlencoded
-//   bodyParser.urlencoded({
-//     extended: true
-//   })
-// );
+app.use(cors());
+
+app.use(bodyParser.json()); // parse application/json
+app.use(
+  // parse application/x-www-form-urlencoded
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 app.use('/welcome', WelcomeController);
 
-// app.route('/api').get((req, res) => {
-//   res.send({
-//     test: ['hello world']
-//   });
-// });
+app.route('/api').get((req, res) => {
+  res.send({
+    test: ['hello world']
+  });
+});
 
-// app.route('/api').post((req, res) => {
-//   res.send(req.body);
-// });
+app.route('/api').post((req, res) => {
+  res.send(req.body);
+});
 
 // Serve the application at the given port
 app.listen(port, () => {
