122e668baf870dcfe0e95757e15b655a8b08abd1
Earthii <xiao_eric@hotmail.com>
2018-10-07 01:47:38 -0400

    use env variables in database. Also, check database exist

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,18 +1,39 @@
 import Sequelize from 'sequelize';
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
 
-function initDb(){
-    return new Sequelize('database', 'admin', 'nimda', {
-        host: process.env.DB_URL || 'localhost',
-        port: Number(process.env.DB_PORT) || 3306,
-        dialect: 'mysql',
-        pool: {
-          max: 5,
-          min: 0,
-          acquire: 30000,
-          idle: 10000
-        },
-        operatorsAliases: false // mute deprecation warning for string based operators?
-      });
+dotenv.config();
+
+function initDb() {
+  const databaseName = String(process.env.DB_NAME);
+  const db_url = String(process.env.DB_URL);
+  const db_port = Number(process.env.DB_PORT);
+  const db_user = String(process.env.DB_USER);
+  const db_pass = String(process.env.DB_PASS);
+  
+  const conn = mysql.createConnection({
+    host:db_url,
+    user: db_user,
+    password: db_pass,
+    port:db_port
+  });
+
+  conn.query(`CREATE DATABASE IF NOT EXISTS ${databaseName}`, (err, results, fields)=>{
+    conn.end();
+  })
+
+  return new Sequelize(databaseName, db_user, db_pass, {
+    host: process.env.DB_URL || 'localhost',
+    port: Number(process.env.DB_PORT) || 3306,
+    dialect: 'mysql',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+    operatorsAliases: false // mute deprecation warning for string based operators?
+  });
 }
 
 export const sequelize = initDb();
